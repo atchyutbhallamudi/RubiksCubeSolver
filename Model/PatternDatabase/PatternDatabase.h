875ee53f1f191ad6434cd20917831c7de68fc9c0
Atchyut Bhallamudi <atchyutbhallamudi@gmail.com>
2026-07-16 02:41:18 +0530

    "Added RubiksCube.cpp,RubiksCube.h code"

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,18 @@
+//
+// Created by atchy on 06-07-2026.
+//
+
+#ifndef RUBIKSCUBESOLVER_PATTERNDATABASE_H
+#define RUBIKSCUBESOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+#endif //RUBIKSCUBESOLVER_PATTERNDATABASE_H
