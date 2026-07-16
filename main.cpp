a10ce902c3097e0084253c6f85b2c70da07a443a
Atchyut Bhallamudi <atchyutbhallamudi@gmail.com>
2026-07-16 16:57:22 +0530

    main function modified

--- main.cpp
+++ main.cpp
@@ -1,287 +1,268 @@
 #include <bits/stdc++.h>
-// #include "Model/RubiksCube3dArray.cpp"
-// #include "Model/RubiksCube1dArray.cpp"
-// #include "Model/RubiksCubeBitboard.cpp"
+#include "Model/RubiksCube3dArray.cpp"
+#include "Model/RubiksCube1dArray.cpp"
 #include "Solver/DFSSolver.h"
 #include "Solver/BFSSolver.h"
 #include "Solver/IDDFSSolver.h"
 #include "Solver/IDAstarSolver.h"
-//#include "PatternDatabases/CornerPatternDatabase.h"
+#include "PatternDatabases/CornerPatternDatabase.h"
 #include "PatternDatabases/CornerDBMaker.h"
 
 using namespace std;
 
 int main() {
-    // RubiksCube3dArray object3DArray;
-    // RubiksCube1dArray object1dArray;
-    // RubiksCubeBitboard objectBitboard;
-    //
-    // object3DArray.print();
-    // object1dArray.print();
-    // objectBitboard.print();
-//
-//    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-    // objectBitboard.u();
-    // object3DArray.u();
-    // object1dArray.u();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
-//
-//    objectBitboard.l();
-//    object3DArray.l();
-//    object1dArray.l();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.f();
-//    object3DArray.f();
-//    object1dArray.f();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.r();
-//    object3DArray.r();
-//    object1dArray.r();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.b();
-//    object3DArray.b();
-//    object1dArray.b();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.d();
-//    object3DArray.d();
-//    object1dArray.d();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    objectBitboard.dPrime();
-//    object3DArray.dPrime();
-//    object1dArray.dPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.bPrime();
-//    object3DArray.bPrime();
-//    object1dArray.bPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.rPrime();
-//    object3DArray.rPrime();
-//    object1dArray.rPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.fPrime();
-//    object3DArray.fPrime();
-//    object1dArray.fPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.lPrime();
-//    object3DArray.lPrime();
-//    object1dArray.lPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    objectBitboard.uPrime();
-//    object3DArray.uPrime();
-//    object1dArray.uPrime();
-//    objectBitboard.print();
-//    object3DArray.print();
-//    object1dArray.print();
-//
-//    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
-//
-//    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
-//    else cout << "NOT SOLVED\n\n";
 
-    // Create two Cubes ------------------------------------------------------------------------------------------
+    // ================= STEP 1: Creation, print, isSolved =================
+    /*
+    RubiksCube3dArray object3DArray;
+    RubiksCube1dArray object1dArray;
+    RubiksCubeBitboard objectBitboard;
 
-//    RubiksCube3dArray cube1;
-//    RubiksCube3dArray cube2;
+    object3DArray.print();
+    object1dArray.print();
+    objectBitboard.print();
 
-//    RubiksCube1dArray cube1;
-//    RubiksCube1dArray cube2;
+    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
+    else cout << "NOT SOLVED\n\n";
 
-//    RubiksCubeBitboard cube1;
-//    RubiksCubeBitboard cube2;
+    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
+    else cout << "NOT SOLVED\n\n";
+
+    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
+    else cout << "NOT SOLVED\n\n";
+    */
 
 
-//  Equality and assignment of cubes --------------------------------------------------------------------------
-
-//    if(cube1 == cube2) cout << "Is equal\n";
-//    else cout << "Not Equal\n";
-//
-//    cube1.randomShuffleCube(1);
-//
-//    if(cube1 == cube2) cout << "Is equal\n";
-//    else cout << "Not Equal\n";
-//
-//    cube2 = cube1;
-//
-//    if(cube1 == cube2) cout << "Is equal\n";
-//    else cout << "Not Equal\n";
+    // ================= STEP 2: Single move (u) consistency =================
+    /*
+    objectBitboard.u();
+    object3DArray.u();
+    object1dArray.u();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+    */
 
 
-//  Unordered_map of Cubes  ------------------------------------------------------------------------------------
+    // ================= STEP 3: Full move cycle + inverse =================
+
+    /*
+    objectBitboard.l();
+    object3DArray.l();
+    object1dArray.l();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    objectBitboard.f();
+    object3DArray.f();
+    object1dArray.f();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    objectBitboard.r();
+    object3DArray.r();
+    object1dArray.r();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    objectBitboard.b();
+    object3DArray.b();
+    object1dArray.b();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    objectBitboard.d();
+    object3DArray.d();
+    object1dArray.d();
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    if (object3DArray.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+    if (object1dArray.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+    if (objectBitboard.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+
+    objectBitboard.dPrime();
+    object3DArray.dPrime();
+    object1dArray.dPrime();
+
+    objectBitboard.bPrime();
+    object3DArray.bPrime();
+    object1dArray.bPrime();
+
+    objectBitboard.rPrime();
+    object3DArray.rPrime();
+    object1dArray.rPrime();
+
+    objectBitboard.fPrime();
+    object3DArray.fPrime();
+    object1dArray.fPrime();
+
+    objectBitboard.lPrime();
+    object3DArray.lPrime();
+    object1dArray.lPrime();
+
+    objectBitboard.uPrime();
+    object3DArray.uPrime();
+    object1dArray.uPrime();
+
+    objectBitboard.print();
+    object3DArray.print();
+    object1dArray.print();
+
+    if (object3DArray.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+    if (object1dArray.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+    if (objectBitboard.isSolved()) cout << "SOLVED\n\n"; else cout << "NOT SOLVED\n\n";
+    */
 
 
-//    unordered_map<RubiksCube3dArray, bool, Hash3d> mp1;
+    // ================= STEP 4: Equality & assignment =================
+    /*
+    RubiksCube3dArray cube1;
+    RubiksCube3dArray cube2;
 
-//    unordered_map<RubiksCube1dArray, bool, Hash1d> mp1;
+    if (cube1 == cube2) cout << "Is equal\n"; else cout << "Not Equal\n";
 
-//    unordered_map<RubiksCubeBitboard, bool, HashBitboard> mp1;
-//
-//    mp1[cube1] = true;
-//    cube2.randomShuffleCube(8);
-//    if (mp1[cube1]) cout << "Cube1 is present\n";
-//    else cout << "Cube1 is not present\n";
-//
-//    if (mp1[cube2]) cout << "Cube2 is present\n";
-//    else cout << "Cube2 is not present\n";
-//
+    cube1.randomShuffleCube(1);
 
-// DFS Solver Testing __________________________________________________________________________________________
-//    RubiksCube3dArray cube;
-//    cube.print();
-//
-//    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
-//    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    cube.print();
-//
-//    DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 8);
-//    vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
-//
-//    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    dfsSolver.rubiksCube.print();
+    if (cube1 == cube2) cout << "Is equal\n"; else cout << "Not Equal\n";
+
+    cube2 = cube1;
+
+    if (cube1 == cube2) cout << "Is equal\n"; else cout << "Not Equal\n";
+    */
 
 
-//BFS Solver -----------------------------------------------------------------------------------------------------
-//    RubiksCubeBitboard cube;
-//    cube.print();
-//
-//    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
-//    for(auto move: shuffle_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    cube.print();
-//
-//    BFSSolver<RubiksCubeBitboard, HashBitboard> bfsSolver(cube);
-//    vector<RubiksCube::MOVE> solve_moves = bfsSolver.solve();
-//
-//    for(auto move: solve_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    bfsSolver.rubiksCube.print();
+    // ================= STEP 5: Hash / unordered_map test =================
 
-// IDDFS Solver ----------------------------------------------------------------------------------------------------
-//    RubiksCubeBitboard cube;
-//    cube.print();
-//
-//    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(7);
-//    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    cube.print();
-//
-//    IDDFSSolver<RubiksCubeBitboard, HashBitboard> iddfsSolver(cube, 7);
-//    vector<RubiksCube::MOVE> solve_moves = iddfsSolver.solve();
-//
-//    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    iddfsSolver.rubiksCube.print();
+    /*
+    unordered_map<RubiksCube3dArray, bool, Hash3d> mp1;
 
-// IDA* SOLVER ---------------------------------------------------------------------------------------------------
-//    RubiksCubeBitboard cube;
-//    cube.print();
-//
-//    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(5);
-//    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    cube.print();
-//
-//    IDAstarSolver<RubiksCubeBitboard, HashBitboard> idAstarSolver(cube);
-//    vector<RubiksCube::MOVE> solve_moves = idAstarSolver.solve();
-//    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    idAstarSolver.rubiksCube.print();
+    mp1[cube1] = true;
+    cube2.randomShuffleCube(8);
 
-// CornerPatternDatabase Testing ---------------------------------------------------------------------------------
-
-//    CornerPatternDatabase cornerDB;
-//    RubiksCubeBitboard cube;
-//    cube.print();
-//
-//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
-//
-//    cornerDB.setNumMoves(cube, 5);
-//
-//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
-//
-//    cube.randomShuffleCube(1);
-//    cube.print();
-//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
-//
-//    cornerDB.setNumMoves(cube, 6);
-//
-//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
+    if (mp1[cube1]) cout << "Cube1 is present\n"; else cout << "Cube1 is not present\n";
+    if (mp1[cube2]) cout << "Cube2 is present\n"; else cout << "Cube2 is not present\n";
+    */
 
 
-// CornerDBMaker Testing --------------------------------------------------------------------------
-    string fileName = "Databases/cornerDepth5V1.txt";
+    // ================= STEP 6: DFS Solver (TIMED) =================
+    /*RubiksCube3dArray cube;
+    cube.print();
 
-//    Code to create Corner Database
-//    CornerDBMaker dbMaker(fileName, 0x99);
-//    dbMaker.bfsAndStore();
+    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(8);
+    for (auto move : shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    auto dfs_start = chrono::high_resolution_clock::now();
+
+    DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 10);
+    vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
+
+    auto dfs_end = chrono::high_resolution_clock::now();
+    double dfs_seconds = chrono::duration<double>(dfs_end - dfs_start).count();
+
+    for (auto move : solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    dfsSolver.rubiksCube.print();
+    cout << "DFS solve time: " << dfs_seconds << " seconds\n\n";
+    */
+
+    // ================= STEP 7: BFS Solver (TIMED) =================
+    /*
+    RubiksCubeBitboard cube;
+    cube.print();
+
+    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+    for (auto move : shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    auto bfs_start = chrono::high_resolution_clock::now();
+
+    BFSSolver<RubiksCubeBitboard, HashBitboard> bfsSolver(cube);
+    vector<RubiksCube::MOVE> solve_moves = bfsSolver.solve();
+
+    auto bfs_end = chrono::high_resolution_clock::now();
+    double bfs_seconds = chrono::duration<double>(bfs_end - bfs_start).count();
+
+    for (auto move : solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    bfsSolver.rubiksCube.print();
+    cout << "BFS solve time: " << bfs_seconds << " seconds\n\n";
+
+    */
+
+    // ================= STEP 8: IDDFS Solver (TIMED) =================
+    /*
+    RubiksCubeBitboard cube;
+    cube.print();
+
+    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(8);
+    for (auto move : shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    auto iddfs_start = chrono::high_resolution_clock::now();
+
+    IDDFSSolver<RubiksCubeBitboard, HashBitboard> iddfsSolver(cube, 8);
+    vector<RubiksCube::MOVE> solve_moves = iddfsSolver.solve();
+
+    auto iddfs_end = chrono::high_resolution_clock::now();
+    double iddfs_seconds = chrono::duration<double>(iddfs_end - iddfs_start).count();
+
+    for (auto move : solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    iddfsSolver.rubiksCube.print();
+    cout << "IDDFS solve time: " << iddfs_seconds << " seconds\n\n";
+    */
+
+
+    // ================= STEP 9: Build the pattern database (run ONCE, TIMED) =================
+
+    /*
+    string dbFileName = "Databases/cornerDepth5V1.txt";
+
+    auto db_start = chrono::high_resolution_clock::now();
+
+    CornerDBMaker dbMaker(dbFileName, 0x99);
+    dbMaker.bfsAndStore();
+
+    auto db_end = chrono::high_resolution_clock::now();
+    double db_seconds = chrono::duration<double>(db_end - db_start).count();
+
+    cout << "Database built.\n";
+    cout << "Database generation time: " << db_seconds << " seconds\n\n";
+
+    */
+    // ================= STEP 10: IDA* Solver (uses the generated database, TIMED) =================
+    /*
+    string dbFileName = "Databases/cornerDepth5V1.txt";
 
     RubiksCubeBitboard cube;
     auto shuffleMoves = cube.randomShuffleCube(13);
     cube.print();
-    for (auto move: shuffleMoves) cout << cube.getMove(move) << " ";
+    for (auto move : shuffleMoves) cout << cube.getMove(move) << " ";
     cout << "\n";
 
-    IDAstarSolver<RubiksCubeBitboard, HashBitboard> idaStarSolver(cube, fileName);
+    auto ida_start = chrono::high_resolution_clock::now();
+
+    IDAstarSolver<RubiksCubeBitboard, HashBitboard> idaStarSolver(cube, dbFileName);
     auto moves = idaStarSolver.solve();
 
-    idaStarSolver.rubiksCube.print();
-    for (auto move: moves) cout << cube.getMove(move) << " ";
-    cout << "\n";
+    auto ida_end = chrono::high_resolution_clock::now();
+    double ida_seconds = chrono::duration<double>(ida_end - ida_start).count();
 
+    idaStarSolver.rubiksCube.print();
+    for (auto move : moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cout << "IDA* solve time: " << ida_seconds << " seconds\n\n";
+    */
 
     return 0;
 }
